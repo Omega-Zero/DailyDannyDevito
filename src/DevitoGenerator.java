1b717b9632f248bb7163dffe5cbe2314403b833f
Omega-Zero <36581543+Omega-Zero@users.noreply.github.com>
2019-05-06 11:52:33 -0400

    Timer stuff aded but not working

--- src/DevitoGenerator.java
+++ src/DevitoGenerator.java
@@ -2,12 +2,16 @@ import java.awt.event.*;
 import java.awt.*; 
 import javax.swing.*; 
 import java.applet.*;
-
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class DevitoGenerator extends JFrame implements ActionListener{
 	// frame 
     static JFrame dannyFrame; 
-  
+    
+    Timer dannyTimer = new Timer();
+    
+    
     // main class 
     public static void main(String[] args) { 
  
@@ -23,8 +27,6 @@ public class DevitoGenerator extends JFrame implements ActionListener{
         
         // Jframe domain contraints 
         
-
-
         dannyFrame.setSize(800, 800); 
         dannyFrame.setVisible(true);    
         
@@ -40,8 +42,23 @@ public class DevitoGenerator extends JFrame implements ActionListener{
         dannyPanel.setBounds(900, 500, 100, 100);
         dannyButton.setBounds(400, 100, 100, 50);
         
+        Timer timer = new Timer(); 
+        TimerTask taskManager = new TaskManager(); 
+        timer.schedule(taskManager, 2000, 5000);
+        
         dannyFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        
+        
     } 
+    
+    class TaskManager extends TimerTask { 
+        public int i = 0; 
+        public void run() 
+        { 
+            System.out.println("Timer ran " + ++i); 
+        } 
+    } 
+  
   
     
  
