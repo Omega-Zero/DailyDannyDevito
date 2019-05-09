bae28787eff7adb64fb41c56df8c16998d5ea53e
Barry <bwittman@gmail.com>
2019-05-09 10:20:33 -0400

    Fixed some errors and cleaned up the code.
    - Removed the extra `JFrame` and used `DevitoGenerator` as its own
    `JFrame`.
    - Removed the `implements ActionListener` from `DevitoGenerator`, since
    it didn't have the method it needed.
    - Changed `imageLabel` to set its image icon instead of making a new
    `JLabel` when the button is pressed.
    - Usually, using `DISPOSE_ON_CLOSE` is fine, but all threads must be
    stopped for the program to end.  That's why I added a `WindowAdapter`
    that disposes the `JFrame` when someone tries to close it but also
    cancels the timer.  Otherwise, the timer thread will keep the program
    from ending.
    - Finally, I moved the images to the project directory.  Doing so means
    that you can refer to them using a relative path, not an absolute one.
    An absolute path won't work on anyone else's computer.

--- src/DevitoGenerator.java
+++ src/DevitoGenerator.java
@@ -1,67 +1,83 @@
-import java.awt.event.*; 
-import java.awt.*; 
-import javax.swing.*; 
-import java.applet.*;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.Random;
+import java.util.Timer;
 
-public class DevitoGenerator extends JFrame implements ActionListener{
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+@SuppressWarnings("serial")
+public class DevitoGenerator extends JFrame {
  
-    Timer dannyTimer = new Timer();
+    private Timer dannyTimer = new Timer();
     
-    static Random randomDannyNum = new Random();
+    private Random randomDannyNum = new Random();
     
-    static ImageIcon[] dannyImageArray = {
-    		new ImageIcon("C:\\Users\\Dell User\\eclipse-workspace\\DailyDannyDevito\\src\\classicDannyD.png"),
-    		new ImageIcon("C:\\Users\\Dell User\\eclipse-workspace\\DailyDannyDevito\\src\\classicDannyD1.png"),
-    		new ImageIcon("C:\\Users\\Dell User\\eclipse-workspace\\DailyDannyDevito\\src\\classicDannyD2.png"),
+    private  ImageIcon[] dannyImageArray = {
+    		new ImageIcon("classicDannyD.png"),
+    		new ImageIcon("classicDannyD1.png"),
+    		new ImageIcon("classicDannyD2.png"),
     };
     
     // main class 
     public static void main(String[] args) { 
  
     	
-    	// create a object, button image 
-        DevitoGenerator dannyObj = new DevitoGenerator(); 
-        JButton dannyButton = new JButton("Next Devito"); 
-        
-        
-    	// create a new frame, panel, label
-        JFrame dannyFrame = new JFrame("Danny Window"); 
-        JPanel dannyPanel = new JPanel(); 
-        JLabel imageLabel = new JLabel(dannyImageArray[randomDannyNum.nextInt(dannyImageArray.length)]); 
-        
-        // Jframe domain contraints 
-        
-        dannyFrame.setSize(800, 800); 
-        dannyFrame.setVisible(true);    
-         
-  
-        // add commands
-        dannyFrame.add(dannyButton); 
-        dannyFrame.add(dannyPanel); 
-        dannyFrame.add(imageLabel);
-        imageLabel.setVisible(true);
-        
-        dannyPanel.setBounds(900, 500, 100, 100);
-        dannyButton.setBounds(400, 100, 100, 50);
-        
-//        Timer timer = new Timer(); 
-//        TimerTask taskManager = new TaskManager(); 
-//        timer.schedule(taskManager, 2000, 5000);
-        
-        dannyFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        
-        dannyButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e){
-                JLabel imageLabel = new JLabel(dannyImageArray[randomDannyNum.nextInt(dannyImageArray.length)]);
-                dannyFrame.add(imageLabel);
-                imageLabel.setVisible(true);
-            }
-        });
-        
+    	// Create the DevitoGenerator, which is a new window
+       new DevitoGenerator();
     } 
+    
+    
+    public DevitoGenerator() {
+    	super("Danny Window");
+    	 JButton dannyButton = new JButton("Next Devito"); 
+         
+         
+     	// create a new frame, panel, label
+         JLabel imageLabel = new JLabel(dannyImageArray[randomDannyNum.nextInt(dannyImageArray.length)]); 
+         
+         // Jframe domain contraints 
+         
+         
+          
+   
+         // add commands
+         add(dannyButton, BorderLayout.SOUTH); 
+         add(imageLabel, BorderLayout.CENTER);
+        
+         
+         
+//         Timer timer = new Timer(); 
+//         TimerTask taskManager = new TaskManager(); 
+//         timer.schedule(taskManager, 2000, 5000);
+         
+         setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+         
+         this.addWindowListener(new WindowAdapter() {
+        	 
+        	 public void windowClosing(WindowEvent e) {
+        		 dannyTimer.cancel();
+        		 dispose();        		 
+        	 }
+         });
+         
+         dannyButton.addActionListener(new ActionListener() {
+             public void actionPerformed(ActionEvent e){
+            	 
+            	 imageLabel.setIcon(dannyImageArray[randomDannyNum.nextInt(dannyImageArray.length)]);
+             }
+         });
+         
+         setSize(800, 800); 
+         setVisible(true);    
+         
+    }
+    
 //    
 //    class TaskManager extends TimerTask { 
 //        public int i = 0; 
