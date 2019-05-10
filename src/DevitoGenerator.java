f7e4382422d00a6fb53fdf3ea2d9983256658eda
Omega-Zero <36581543+Omega-Zero@users.noreply.github.com>
2019-05-10 16:49:06 -0400

    Updated comments, tried to get random pic gen in actionlistener to work but still errors

--- src/DevitoGenerator.java
+++ src/DevitoGenerator.java
@@ -41,7 +41,7 @@ public class DevitoGenerator extends JFrame {
     		new ImageIcon("classicDannyD14.png")
     };
     
-   List<Integer> alreadyDevitodArray = Arrays.asList();
+   public List<Integer> alreadyDevitodArray = Arrays.asList();
     
     // main class 
     public static void main(String[] args) { 
@@ -52,21 +52,21 @@ public class DevitoGenerator extends JFrame {
     } 
     
     
-    
+    //Makes the window 
     public DevitoGenerator() {
     	super("Danny Window");
     	
-    	 // button
+    	 // button variable
     	 JButton dannyButton = new JButton("Next Devito"); 
     	 
-     	 // create a new frame, panel, label
+     	 // create a JLabel, put in it to start a random index in the imageArray
          JLabel imageLabel = new JLabel(dannyImageArray[randomDannyNum.nextInt(dannyImageArray.length)]); 
    
-         // add commands
+         // add label and button to the frame(super)
          add(dannyButton, BorderLayout.SOUTH); 
          add(imageLabel, BorderLayout.CENTER);
       
-         
+         //stop program on close
          setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
          
          this.addWindowListener(new WindowAdapter() {
@@ -78,30 +78,34 @@ public class DevitoGenerator extends JFrame {
          });
          
          
-         //button listener
+         //button listener - what happens when button is clicked
          dannyButton.addActionListener(new ActionListener() {
          
         	 public void actionPerformed(ActionEvent e){
             	
             	 int randomDanny = randomDannyNum.nextInt(dannyImageArray.length);
-            	 
-            	 //If the random number is not in the array AKA hasn't been shown yet
-            	 if (!alreadyDevitodArray.contains(randomDanny)) {
+            	
+            	 //If the arrays aren't the same size not all possible images have been shown, so..
+            	 if (alreadyDevitodArray.size() != dannyImageArray.length) {
             		 
-            		 //Display the image
-            		 imageLabel.setIcon(dannyImageArray[randomDanny]);
-            		 //Add that image to the array so it doesn't repeat.
-            		 alreadyDevitodArray.add(randomDanny);
-            		 
-            		 
-            	 }else {
-            		 
-            		  //Choose a new image index
-            		  randomDanny = randomDannyNum.nextInt();
-            		  //Call button listener again
-            		  dannyButton.doClick();
-            	 }
-             }
+	            	 //If the random number is not in the array AKA hasn't been shown yet
+	            	 if (!Arrays.asList(alreadyDevitodArray).contains(randomDanny)) {
+	            		 
+	            		 //Display the image
+	            		 imageLabel.setIcon(dannyImageArray[randomDanny]);
+	            		 //Add that image to the array so it doesn't repeat.
+	            		 alreadyDevitodArray.add(randomDanny);
+	            		 
+	            		 
+	            	 }else{
+	            	//The image has already been displayed so..	 
+	            		  //Choose a new image index
+	            		  randomDanny = randomDannyNum.nextInt();
+	            		  //Call button listener again
+	            		  dannyButton.doClick();
+	            	 }
+	             }
+        	 }
          });
          
          super.setSize(800, 800); 
