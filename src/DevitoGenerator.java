fa38ba6613e9889242caab3ea0f140e2002d1486
Barry <bwittman@gmail.com>
2019-05-19 14:44:10 -0400

    Fixes to make both the timer and the button press work
    - Both the timer and the button call the `updateImage()` method.
    - The `alreadyDisplayed` list keeps track of `ImageIcon` objects that
    have already been displayed.
    - When `updateImage()` is called:
    	- It checks to see if `alreadyDisplayed` is the length of
    `dannyImageArray` and clears it if it is (since all images have been
    shown).
    	- Then it randomly selects an image until it finds one that is *not*
    present in `alreadyDisplayed`.
    	- Finally, it adds that image to `alreadyDisplayed` and sets the icon
    of `imageLabel` to the image.
    	- Note that `updateImage()` is synchronized in case multiple threads
    are accessing it at the same time.
    	- This implementation will occasionally display the same image twice if
    all the images have been shown and the first new image to be shown
    happens to be the last image.
    - Also fixed the timer so that it uses a correctly created `TimerTask`
    object.

--- src/DevitoGenerator.java
+++ src/DevitoGenerator.java
@@ -3,129 +3,125 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.ArrayList;
+
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 
 @SuppressWarnings("serial")
 public class DevitoGenerator extends JFrame {
- 
-    private Timer dannyTimer = new Timer();
-    
-    private Random randomDannyNum = new Random();
-    
-    private  ImageIcon[] dannyImageArray = {
-    		new ImageIcon("classicDannyD0.png"),
-    		new ImageIcon("classicDannyD1.png"),
-    		new ImageIcon("classicDannyD2.png"),
-    		new ImageIcon("classicDannyD3.png"),
-    		new ImageIcon("classicDannyD4.png"),
-    		new ImageIcon("classicDannyD5.png"),
-    		new ImageIcon("classicDannyD6.png"),
-    		new ImageIcon("classicDannyD7.png"),
-    		new ImageIcon("classicDannyD8.png"),
-    		new ImageIcon("classicDannyD9.png"),
-    		new ImageIcon("classicDannyD10.png"),
-    		new ImageIcon("classicDannyD11.png"),
-    		new ImageIcon("classicDannyD12.png"),
-    		new ImageIcon("classicDannyD13.png"),
-    		new ImageIcon("classicDannyD14.png")
-    };
-    
-   public List<Integer> alreadyDevitodArray = Arrays.asList();
-    
-    // main class 
-    public static void main(String[] args) { 
- 
-    	// Create the DevitoGenerator, which is a new window
-       new DevitoGenerator();
-       
-    } 
-    
-    
-    //Makes the window 
-    public DevitoGenerator() {
-    	
-    	super("Danny Window");
-    	
-    	//Set rate at which new devitos appear
-    	dannyTimer.scheduleAtFixedRate(newDevito(), 2000, 5000);
-    
-    	// button variable
-    	 JButton dannyButton = new JButton("Next Devito"); 
-    	 
-     	 // create a JLabel, put in it to start a random index in the imageArray
-         JLabel imageLabel = new JLabel(dannyImageArray[randomDannyNum.nextInt(dannyImageArray.length)]); 
-   
-         // add label and button to the frame(super)
-         add(dannyButton, BorderLayout.SOUTH); 
-         add(imageLabel, BorderLayout.CENTER);
-      
-         //stop program on close
-         setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-         
-         this.addWindowListener(new WindowAdapter() {
-        	 
-        	 public void windowClosing(WindowEvent e) {
-        		 dannyTimer.cancel();
-        		 dispose();        		 
-        	 }
-         });
-         
 
-         
-         //button listener - what happens when button is clicked
-         dannyButton.addActionListener(new ActionListener() {
-         
-        	 public void actionPerformed(ActionEvent e){
-            	
-            	 int randomDanny = randomDannyNum.nextInt(dannyImageArray.length);
-            	
-            	 //If the arrays aren't the same size not all possible images have been shown, so..
-            	 if (alreadyDevitodArray.size() != dannyImageArray.length) {
-            		 
-	            	 //If the random number is not in the array AKA hasn't been shown yet
-	            	 if (!Arrays.asList(alreadyDevitodArray).contains(randomDanny)) {
-	            		 
-	            		 //Display the image
-	            		 imageLabel.setIcon(dannyImageArray[randomDanny]);
-	            		 //Add that image to the array so it doesn't repeat.
-	            		 alreadyDevitodArray.add(randomDanny);
-	            		 
-	            		 
-	            	 }else{
-	            	//The image has already been displayed so..	 
-	            		  //Choose a new image index
-	            		  randomDanny = randomDannyNum.nextInt();
-	            		  //Call button listener again
-	            		  dannyButton.doClick();
-	            	 }
-	             }
-        	 }
-         });
-         
-         
-         
-         super.setSize(800, 800); 
-         setVisible(true);    
-         
-    }
-    
-    public TimerTask newDevito() {
-    	 JLabel imageLabel = new JLabel(dannyImageArray[randomDannyNum.nextInt(dannyImageArray.length)]);
-    	 imageLabel.setIcon(dannyImageArray[randomDannyNum.nextInt(dannyImageArray.length)]);
-    	 return null;
-    }
-    
-    
+	private Timer dannyTimer = new Timer();
+
+	private Random randomDannyNum = new Random();
+
+	private  ImageIcon[] dannyImageArray = {
+			new ImageIcon("classicDannyD0.png"),
+			new ImageIcon("classicDannyD1.png"),
+			new ImageIcon("classicDannyD2.png"),
+			new ImageIcon("classicDannyD3.png"),
+			new ImageIcon("classicDannyD4.png"),
+			new ImageIcon("classicDannyD5.png"),
+			new ImageIcon("classicDannyD6.png"),
+			new ImageIcon("classicDannyD7.png"),
+			new ImageIcon("classicDannyD8.png"),
+			new ImageIcon("classicDannyD9.png"),
+			new ImageIcon("classicDannyD10.png"),
+			new ImageIcon("classicDannyD11.png"),
+			new ImageIcon("classicDannyD12.png"),
+			new ImageIcon("classicDannyD13.png"),
+			new ImageIcon("classicDannyD14.png")
+	};
+
+	private List<ImageIcon> alreadyDisplayed = new ArrayList<ImageIcon>();
+	private JLabel imageLabel;
+
+	// main class 
+	public static void main(String[] args) { 
+
+		// Create the DevitoGenerator, which is a new window
+		new DevitoGenerator();
+
+	} 
+
+
+	//Makes the window 
+	public DevitoGenerator() {
+
+		super("Danny Window");
+
+
+		// button variable
+		JButton dannyButton = new JButton("Next Devito"); 
+
+		// create a JLabel, put in it to start a random index in the imageArray
+		imageLabel = new JLabel(dannyImageArray[randomDannyNum.nextInt(dannyImageArray.length)]); 
+
+		// add label and button to the frame(super)
+		add(dannyButton, BorderLayout.SOUTH); 
+		add(imageLabel, BorderLayout.CENTER);
+
+		//stop program on close
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+
+		this.addWindowListener(new WindowAdapter() {
+
+			public void windowClosing(WindowEvent e) {
+				dannyTimer.cancel();
+				dispose();        		 
+			}
+		});
+
+		//button listener - what happens when button is clicked
+		dannyButton.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e){
+				updateImage();            	 
+			}
+		});
+
+
+
+		super.setSize(800, 800); 
+		setVisible(true); 
+		
+
+		//Set rate at which new devitos appear
+		dannyTimer.scheduleAtFixedRate(new TimerTask() {
+
+			//The timer task also updates the image
+			@Override
+			public void run() {
+				updateImage();			
+			}
+			
+			
+		}, 2000, 5000);
+	}
+
+	public synchronized void updateImage() {
+		
+		//If the visited list is the same size, then all the images have already been visited, so we have to clear the list
+		if (alreadyDisplayed.size() == dannyImageArray.length) 
+			alreadyDisplayed.clear();
+
+		ImageIcon randomDanny;
+
+		//Search for unvisited image   		
+		do
+		{
+			randomDanny = dannyImageArray[randomDannyNum.nextInt(dannyImageArray.length)];   			
+		} while (alreadyDisplayed.contains(randomDanny));
+
+		//add selected image
+		alreadyDisplayed.add(randomDanny);
+		imageLabel.setIcon(randomDanny);
+	}
 }
